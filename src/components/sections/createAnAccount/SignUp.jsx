32e33eb2fbd48c1d52ca8ff5b618676b37df1894
Gourav Makode <140704003+GT0SRT@users.noreply.github.com>
2024-12-08 15:20:14 +0530

    Update SignUp.jsx

--- src/components/sections/createAnAccount/SignUp.jsx
+++ src/components/sections/createAnAccount/SignUp.jsx
@@ -1,13 +1,12 @@
 import { useNavigate } from "react-router";
-
 import {useRef} from 'react'
 import { Link } from "react-router-dom";
+import { useSelector } from 'react-redux';
 
 export default function SignUp() {
   const navigate = useNavigate();
   const username = useRef();
   const password = useRef();
-
 
   function onHandleUserName(event) {
     username.current = event.target.value;
@@ -17,12 +16,18 @@ export default function SignUp() {
     password.current= event.target.value;
   }
 
+  function classNames(...classes) {
+    return classes.filter(Boolean).join(' ')
+  }
+
+  const isClicked = useSelector((state) => state.click_redux_slice.isClicked);
+
   return (
     <>
       <div className="flex bg-gradient-to-r from-blue-900 via-pink-400 to-indigo-700 h-screen w-screen flex-col justify-center items-center">
-        <div className="bg-white flex flex-col justify-center items-center rounded-xl shadow-3xl w-fit p-7">
+        <div className={classNames( isClicked ? 'bg-white':'bg-gray-900','flex flex-col justify-center items-center rounded-xl shadow-3xl w-fit p-7',)}>
           <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            <h2 className={classNames( isClicked ? 'text-grsy-900':'text-white', 'mt-10 text-center text-2xl font-bold leading-9 tracking-tight',)}>
               Sign up to our Application
             </h2>
           </div>
@@ -32,7 +37,7 @@ export default function SignUp() {
               <div>
                 <label
                   htmlFor="email"
-                  className="block text-sm font-medium leading-6 text-gray-900"
+                  className={classNames( isClicked ? 'text-gray-900':'text-white', 'block text-sm font-medium leading-6',)}
                 >
                   Email address
                 </label>
@@ -44,7 +49,7 @@ export default function SignUp() {
                     required
                     onInput={onHandleUserName}
                     autoComplete="email"
-                    className="block p-3 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 focus:outline-none sm:text-sm sm:leading-6"
+                    className={classNames( isClicked ? 'text-gray-900':'text-white bg-gray-900','block p-3 w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 focus:outline-none sm:text-sm sm:leading-6',)}
                   />
                 </div>
               </div>
@@ -53,7 +58,7 @@ export default function SignUp() {
                 <div className="flex items-center justify-between">
                   <label
                     htmlFor="password"
-                    className="block text-sm font-medium leading-6 text-gray-900"
+                    className={classNames( isClicked ? 'text-gray-900':'text-white', 'block text-sm font-medium leading-6',)}
                   >
                     Password
                   </label>
@@ -66,13 +71,13 @@ export default function SignUp() {
                     required
                     onInput={onHandleUserPassword}
                     autoComplete="current-password"
-                    className="block p-3 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 focus:outline-none sm:text-sm sm:leading-6"
+                    className={classNames( isClicked ? 'text-gray-900':'text-white bg-gray-900','block p-3 w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 focus:outline-none sm:text-sm sm:leading-6',)}
                   />
                 </div>
                 <div className="flex items-center justify-between mt-4">
                   <label
                     htmlFor="password"
-                    className="block text-sm font-medium leading-6 text-gray-900"
+                    className={classNames( isClicked ? 'text-gray-900':'text-white', 'block text-sm font-medium leading-6',)}
                   >
                     Confirm Password
                   </label>
@@ -88,7 +93,7 @@ export default function SignUp() {
                     required
                     onInput={onHandleUserPassword}
                     autoComplete="confirm-password"
-                    className="block p-3 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 focus:outline-none sm:text-sm sm:leading-6"
+                    className={classNames( isClicked ? 'text-gray-900':'text-white bg-gray-900','block p-3 w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 focus:outline-none sm:text-sm sm:leading-6',)}
                   />
                 </div>
               </div>
@@ -96,7 +101,7 @@ export default function SignUp() {
               <div>
                 <button
                   type="button"
-                  className="flex w-full justify-center rounded-md bg-red-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-white hover:text-red-600 border-2 border-red-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
+                  className={classNames(isClicked ? 'hover:bg-white': 'hover:bg-gray-900','flex w-full justify-center rounded-md bg-red-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:text-red-600 border-2 border-red-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600',)}
                 >
                 Confirm & Create Account
                 </button>
